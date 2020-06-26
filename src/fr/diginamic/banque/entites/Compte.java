c2853f10f51c5e11689f7dea22d6b77b8c91435c
Jeremy CINELLI <jeremy.cinelli@gmail.com>
2020-06-26 14:14:41 +0200

    Approche Objet Java / TP V5

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,37 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+
+	String numCompte;
+	double soldeCompte;
+
+//	CONSTRUTOR
+	public Compte(String numCompte, double soldeCompte) {
+		this.numCompte = numCompte;
+		this.soldeCompte = soldeCompte;
+	}
+
+//	GETTER & SETTER
+	public String getNumCompte() {
+		return numCompte;
+	}
+
+	public void setNumCompte(String numCompte) {
+		this.numCompte = numCompte;
+	}
+
+	public double getSoldeCompte() {
+		return soldeCompte;
+	}
+
+	public void setSoldeCompte(double soldeCompte) {
+		this.soldeCompte = soldeCompte;
+	}
+
+	@Override
+	public String toString() {
+		return "Numéro de compte = " + numCompte + "\rSolde du compte = " + soldeCompte;
+	}
+	
+
+}
